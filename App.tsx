976c7315e3a306c929fa5f3f3531ad0de6c7175f
EmilyGMG <emily.maria2010@hotmail.com>
2023-07-20 16:31:39 -0300

    modified version of styled-components and added loading before font loaded

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import Home from '@screens/Home';
-import theme from '@theme/index';
+import theme from './src/theme';
 import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito'; 
 import { ThemeProvider } from 'styled-components';
-import { ActivityIndicator } from 'react-native';
+
+import Loading from '@components/Loading';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Nunito_400Regular, Nunito_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Home /> : <ActivityIndicator />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </ThemeProvider>
   );
 }
