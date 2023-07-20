6699978f3079eead1da519ea5058ecf9fd802ab7
EmilyGMG <emily.maria2010@hotmail.com>
2023-07-20 16:33:38 -0300

    added modified statusBar

--- App.tsx
+++ App.tsx
@@ -4,12 +4,18 @@ import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/
 import { ThemeProvider } from 'styled-components';
 
 import Loading from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Nunito_400Regular, Nunito_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle='dark-content'
+        backgroundColor='transparent'
+        translucent
+      />
       {fontsLoaded ? <Home /> : <Loading />}
     </ThemeProvider>
   );
