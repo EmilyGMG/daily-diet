0e80eef78e57c2d789566a29326435de2ee03120
EmilyGMG <emily.maria2010@hotmail.com>
2023-07-20 15:59:16 -0300

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import Home from '@screens/Home';
 import theme from '@theme/index';
+import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito'; 
 import { ThemeProvider } from 'styled-components';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({ Nunito_400Regular, Nunito_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      {fontsLoaded ? <Home /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
